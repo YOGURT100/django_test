c7ea4faff7c7fce309cd15a416a0538bc9eb4f0f
Fers <onkefir@gmail.com>
2024-02-02 12:41:57 +0300

    добавил catalog.html and product.html

--- goods/urls.py
+++ goods/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from main import views
+from goods import views
 
-app_name = "main"
+app_name = "goods"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
+    path("", views.catalog, name="index"),
+    path("product/", views.product, name="product"),
 ]
